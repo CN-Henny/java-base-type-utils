f22ef707d97f45cb52156de99ffa61db9af8802e
Henny <351520965@qq.com>
2022-10-18 21:01:29 +0800

    增加扩展类的扩展函数

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 
+import java.io.File;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.*;
@@ -12,27 +13,34 @@ public class DemoApplication {
         Integer aasd = 32767;
         Double a = 1234567890123.123456789456;
         BigDecimal c = new BigDecimal("0.0000000000000000000000000");
+        UserData d = new UserData();
+        //a.systemCustomGetModifyContent();
+        //c.systemCustomGetModifyContent();
+        //d.systemCustomGetModifyContent();
         Integer b = 0;
         Date aaa;
-//a.toInteger();
+        //a.toInteger();
         System.out.println(a.customDecimalsLength());
-System.out.println(a.customRoundHalfUp(2));
+        System.out.println(a.customRoundHalfUp(2));
         System.out.println(a.customRoundHalfDown(2));
         System.out.println(a.customRoundUp(2).customNousedF_E());
         System.out.println(a.customRoundDown(2));
         System.out.println(c);
         System.out.println(c);
         //Date c = new Date();
-//a.toInteger();
+        //a.toInteger();
         //a.toString().toBigDecimal();
-        Map<String,String> arr = new HashMap<>();
-arr.addaaa("1","1").addaaa("2","2");
+        Map<String, String> arr = new HashMap<>();
+        arr.addaaa("1", "1").addaaa("2", "2");
         String aewew = "";
-        String[] strings = new String[] {"a", "b", "c"};
+        String[] strings = new String[]{"a", "b", "c"};
         ArrayList<UserData> arrayList = new ArrayList<>();
         List<Integer> ids = new ArrayList<>();
         strings.toaaaList();
-        arrayList.customAdd(1);
+        ids.customAdd(1).customAdd(2).customAdd(3);
+        //ids.add(1).add(2);
+        //File asdasd = new File();
+        //asdasd.file();
     }
 
 }
---
+++ src/main/java/extensions/java/io/File/FileExtension.java
@@ -0,0 +1,15 @@
+package extensions.java.io.File;
+
+import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.Self;
+import manifold.ext.rt.api.This;
+
+import java.io.File;
+import java.util.List;
+
+@Extension
+public class FileExtension {
+
+    public static void file(@This File a) {
+    }
+}
